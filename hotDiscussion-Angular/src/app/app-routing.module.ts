32f9fa13d22831a0d316ffb3873621f140d95dc6
Nicky <zhudtqzz@hotmail.com>
2019-08-10 18:29:35 +1000

    basic file structure (Angular + Nodejs ) directory

---
+++ hotDiscussion-Angular/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LogInComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
