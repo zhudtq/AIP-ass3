548906ae8666dac0a5a2a6d9df31f27864ccf405
QuanZhu <zhudtqzz@gmail.com>
2019-08-22 22:17:02 +1000

    node - using cors

--- hotDiscussion-NodeJs/src/index.js
+++ hotDiscussion-NodeJs/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require('./mongoDb/dbConnection')
+const cors = require('cors')
 const loginRouter = require('./routers/user')
 
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(express.json())
 app.use(loginRouter)
 
