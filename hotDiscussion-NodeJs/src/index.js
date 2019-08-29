92de931296daa25d1a6f05867e5e65790211d67c
QuanZhu <zhudtqzz@gmail.com>
2019-08-30 01:20:58 +1000

    node - image upload

--- hotDiscussion-NodeJs/src/index.js
+++ hotDiscussion-NodeJs/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./mongoDb/dbConnection')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const userRouter = require('./routers/user')
 const chatRouter = require('./routers/chat')
@@ -11,6 +12,7 @@ const port = 3000
 app.use(cors())
 
 app.use(express.json())
+// app.use(bodyParser.urlencoded({extended: true}))
 app.use(userRouter)
 app.use(chatRouter)
 
--- hotDiscussion-NodeJs/src/routers/chat.js
+++ hotDiscussion-NodeJs/src/routers/chat.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const Chat = require('../models/chatModel')
 const auth = require('../middleware/authentication')
+const multer = require('multer')
 
 const router = new express.Router()
+
+const storage = multer({dest: 'me/'})
 
 router.post('/chats', async (req, res) => {
     try {
@@ -16,6 +19,29 @@ router.post('/chats', async (req, res) => {
     catch (e) {
         res.status(400).send(e)
     }
+})
+
+router.post('/images', storage.single('image'), async (req, res) => {
+    console.log('image coming')
+    // try {
+    //     if (!req.file){
+    //         console.log('no file received...')
+    //         return res.send({
+    //         message: false
+    //     })
+    //     }
+    //     else {
+    //         console.log('file received')
+    //         return res.send({
+    //             message: true
+    //         })
+    //     }
+    // }
+    // catch (e) {
+
+    // }
+    console.log(req.file)
+    res.send({mes: true})
 })
 
 module.exports = router
