e9e80cbc510a4a7090694c5d5729275df50291aa
Huiyun <niuhy0728@gmail.com>
2019-09-17 14:15:07 +1000

    manage conflict

--- hotDiscussion-Angular/src/app/pages/chattingPage/profile/profile.component.ts
+++ hotDiscussion-Angular/src/app/pages/chattingPage/profile/profile.component.ts
@@ -2,10 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { UploadProfileService } from '../../../http/upload-profile.service';
 import { HttpClient } from '@angular/common/http';
 
-// class ProfilePhoto {
-  
-//   constructor(public file: File) {}
-// }
 
 @Component({
   selector: 'app-profile',
@@ -14,13 +10,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProfileComponent implements OnInit {
 
-  //selectedFile: ProfilePhoto; 
+  
   fileData: File = null;
 constructor(private http: HttpClient, private uploadProfileService: UploadProfileService) { }
  
-// fileProgress(fileInput: any) {
-//     this.fileData = <File>fileInput.target.files[0];
-// }
 
 onFilePicked(event){
  console.log("onfilepicked event")
@@ -34,7 +27,6 @@ onSubmit() {
     const formData = new FormData();
     formData.append('avatar', this.fileData);
     this.uploadProfileService.uploadImage(formData)
-    //return this.http.post('http://localhost:3000/users/me/avatar', formData)
       .subscribe((data) => {
                 console.log('success')
                 alert('upload!')
@@ -49,27 +41,5 @@ onSubmit() {
   
   ngOnInit() {
   }
-  // processFile(imageInput: any) {
-  //   let file: File = imageInput.files[0];
-  //   const reader = new FileReader();
 
-  //   //reader.addEventListener('load', (event: any) => {
-
-  //     //this.selectedFile = new ProfilePhoto(file);
-  //     let imageFile = new FormData();
-  //     imageFile.append('image', file)
-
-  //     this.uploadProfileService.uploadImage(imageFile).subscribe(
-  //       (data) => {
-  //         console.log('success')
-  //         alert('upload!')
-  //       },
-  //       (error) => {
-  //         alert('fail'+ error)
-  //         console.log(error);
-  //       })
-  //   };
-
-    //reader.readAsDataURL(file);
-  //}
 }
